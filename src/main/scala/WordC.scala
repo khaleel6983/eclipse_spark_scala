0397935d0d275e2f843f43836ab133a5b28ab6b1
khaleel6983 <khaleel6983@gmail.com>
2020-08-31 22:12:18 +0530

    Add all my files

---
+++ src/main/scala/WordC.scala
@@ -0,0 +1,11 @@
+impo
+
+object WordC {
+  
+  def main(args:main [String]){
+    val SparkConf = new SparkConf;
+    val sc = sparkContext(SparkConf).setMaster("local").setAppName("WordC");
+    
+    
+  }
+}
